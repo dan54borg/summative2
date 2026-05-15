d7f2b928bb7f542843a8f7b0f98af157711e7f28
Dan Borg <dan54borg@gmail.com>
2026-05-15 20:30:24 +0100

    1st_test

--- test_smoke_and_all.py
+++ test_smoke_and_all.py
@@ -1,18 +1,21 @@
 import unittest # importing the testing framework
 from unittest.mock import patch 
-
+from main import KnifeSafety
 
 class SmokeTest(unittest.TestCase):
     def setUp(self): 
-        self.app = () 
+        self.app = KnifeSafety() 
     
     def test_ut_works(self):
         self.assertEqual(2+2,4)
         self.assertTrue(1)
         self.assertFalse(0)
     
-  
+    def test_name_happy(self):
+        result = self.app.display_output("John")
+        self.assertEqual(result, "OK")
 
+      
     
 if __name__ == "__main__":
     unittest.main()
