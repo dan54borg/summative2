845fde0a8a77e63c0dc0b6d9f8f864c81e3cead1
Dan Borg <dan54borg@gmail.com>
2026-05-15 20:43:15 +0100

    addeding additional tests

--- test_smoke_and_all.py
+++ test_smoke_and_all.py
@@ -12,18 +12,29 @@ class SmokeTest(unittest.TestCase):
         self.assertFalse(0)
     
     def test_name_happy(self):
+        #Testing names are accepted
         result = self.app.display_output("John")
         self.assertEqual(result, "OK")
 
     def test_name_happy_edge(self):
+        #Testing that characters are accepted
         result = self.app.display_output("O'Toole")
         self.assertEqual(result, "OK")
+        #Testing the length of a name
+        result = self.app.display_output("Christopher Walken")
+        self.assertEqual(result,"OK")
+        result = self.app
 
     @patch('tkinter.messagebox.showerror') 
     def test_name_fail_presence(self, mock_error):
+        #Testing that leaving the name blank will show up the correct error
         self.app.display_output("")
         mock_error.assert_called_with("Error", "Name cannot be left blank")
-    
+        #Testing the error for characters
+        self.app.display_output("Dan1el")
+        mock_error.assert_called_with("Error", "The name should not have any numbers")
+         
+
 
 
       
